66004d7e612519465d0ba4aec2f9e92836c74790
零 <kanade@acgme.cn>
2022-08-16 10:29:28 +0800

    feat: update

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ fi
 
 echo 'Start to install HandyGCCS for Ayaneo Air';
 cd HandyGCCS;
-git switch aya-air;
+git switch master;
 sudo make install;
 cd ../;
 echo 'Installation finished: HandyGCCS';
@@ -41,12 +41,12 @@ echo 'Installation finished: HandyGCCS';
 
 if [ ! -f 'gamescope' ]; then
   echo 'Start to get binary: gamescope';
-  wget -c https://git.futo.design/zero/aya-air-script-steamos/-/raw/offline/binary/gamescope?inline=false;
+  wget -c https://git.futo.design/zero/aya-air-script-steamos/-/raw/offline/binary/gamescope;
 else
   echo 'Binary exists: gamescope';
 fi
 
-echo 'Start to install gamescope-plus for Ayaneo Air';
+echo 'Start to install gamescope for Ayaneo Air';
 # cd gamescope-plus;
 # git switch master;
 # git submodule update --init;
@@ -57,7 +57,7 @@ echo 'Start to install gamescope-plus for Ayaneo Air';
 # sudo cp /usr/local/bin/gamescope /usr/bin/;
 # cd ../;
 sudo cp ./gamescope /usr/bin/;
-echo 'Installation finished: gamescope-plus';
+echo 'Installation finished: gamescope';
 
 if [ ! -d 'gamescope-session' ]; then
   echo 'Start to clone repository: ruineka/gamescope-session';
