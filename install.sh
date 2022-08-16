4c94733e3fb44c2c82e9fa042d26b255f505d482
零 <kanade@acgme.cn>
2022-08-16 10:51:36 +0800

    feat: update

--- install.sh
+++ install.sh
@@ -3,79 +3,58 @@
 clear;
 
 echo '';
-echo '----------------------------------------------------------------';
-echo '| Post install script for Ayaneo Air w/ Steam OS (HoloISO).    |';
-echo '| Script by YoumuKonpaku (https://youmukonpaku.com)            |';
-echo '----------------------------------------------------------------';
+echo '-------------------------------------------------------';
+echo '| 当前脚本可以修复Air在SteamOS(HoloISO)中出现的一些问题     |';
+echo '| 脚本作者: 魂魄妖梦 (https://youmukonpaku.com)          |';
+echo '-------------------------------------------------------';
 echo '';
 
-echo 'Pre-installation start...';
-sudo pacman -Sy meson ninja cmake python3 dbus-python vulkan-headers;
-sudo pacman -Sy --needed base-devel git
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur
-makepkg -fsri
-cd ../
-echo 'Pre-installation end...';
-
-if [ ! -d 'HandyGCCS' ]; then
-  echo 'Start to clone repository: ShadowBlip/HandyGCCS';
-  git clone 'https://github.com/ShadowBlip/HandyGCCS.git';
-else
-  echo 'Repository exists: ShadowBlip/HandyGCCS';
-fi
-
-echo 'Start to install HandyGCCS for Ayaneo Air';
-cd HandyGCCS;
-git switch master;
-sudo make install;
+echo '开始安装基础依赖...';
+sudo pacman -Sy --needed base-devel git cmake python3 meson ninja dbus-python vulkan-headers;
+git clone https://aur.archlinux.org/pikaur.git;
+cd pikaur;
+makepkg -fsri;
 cd ../;
-echo 'Installation finished: HandyGCCS';
+echo '基础依赖安装完成...';
 
-# if [ ! -d 'gamescope-plus' ]; then
-#   echo 'Start to clone repository: ruineka/gamescope-plus';
-#   git clone 'https://github.com/ruineka/gamescope-plus.git';
-# else
-#   echo 'Repository exists: ruineka/gamescope-plus';
-# fi
+echo '开始安装HandyGCCS...';
+echo '备注: HandyGCCS可以修复Air功能键失效的问题.';
+pikaur -Sy handygccs-git;
+sudo udevadm control -R;
+sudo systemctl enable handycon && sudo systemctl start handycon;
+echo 'HandyGCCS安装完成...';
 
 if [ ! -f 'gamescope' ]; then
-  echo 'Start to get binary: gamescope';
+  echo 'gamescope二进制文件不存在, 开始下载...';
   wget -c https://git.futo.design/zero/aya-air-script-steamos/-/raw/offline/binary/gamescope;
 else
-  echo 'Binary exists: gamescope';
+  echo 'gamescope二进制文件已存在, 跳过下载...';
 fi
 
-echo 'Start to install gamescope for Ayaneo Air';
-# cd gamescope-plus;
-# git switch master;
-# git submodule update --init;
-# git switch gamescope-plus;
-# meson build/;
-# ninja -C build/;
-# sudo meson install -C build/ --skip-subprojects;
-# sudo cp /usr/local/bin/gamescope /usr/bin/;
-# cd ../;
+echo '开始安装gamescope...';
+echo '备注: gamescope可以修复Air屏幕倒转的问题...';
 sudo cp ./gamescope /usr/bin/;
-echo 'Installation finished: gamescope';
+echo 'gamescope安装完成...';
 
 if [ ! -d 'gamescope-session' ]; then
-  echo 'Start to clone repository: ruineka/gamescope-session';
+  echo 'gamescope-session目录不存在, 开始下载...';
   git clone 'https://github.com/ruineka/gamescope-session.git';
 else
-  echo 'Repository exists: ruineka/gamescope-session';
+  echo 'gamescope-session目录已存在, 跳过下载...';
 fi
 
-echo 'Start to install gamescope-session for Ayaneo Air';
+echo '开始安装gamescope-session...';
+echo '备注: gamescope-session可以配合修复Air屏幕倒转的问题...';
 cd gamescope-session;
 sudo cp -f -r ./etc /;
 sudo cp -f -r ./usr /;
 cd ../;
-echo 'Installation finished: gamescope-session';
+echo 'gamescope-session安装完成...';
 
-echo 'Start to install rz608-fix-git for Ayaneo Air';
+echo '开始安装rz608-fix-git...';
+echo '备注: rz608-fix-git可以修复Air息屏唤起后丢失Wi-Fi驱动的问题...';
 sudo pikaur -S rz608-fix-git
 sudo udevadm control -R
-echo 'Installation finished: rz608-fix-git';
+echo 'rz608-fix-git安装完成...';
 
-echo 'All operations has done, enjoy your Ayaneo Air!';
+echo '所有操作均已完成, 尽情享受吧!';
